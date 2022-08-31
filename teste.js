e257e934c71472e8b4963a13297bc570b1726170
Wilber Ribeiro <wilbergoias@gmail.com>
2022-08-31 11:29:17 -0300

    entendendo uso de variáveis em java (facil facil)

---
+++ teste.js
@@ -0,0 +1,4 @@
+const nomeCliente = "Wilber";
+const boasvindas = "Olá " + nomeCliente + ", estamos gratos de receber sua visita, por favor avalie nosso site no final do seu uso";
+
+console.log(boasvindas);
